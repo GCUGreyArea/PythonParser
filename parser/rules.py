1d9563690958ea66724f05ee99f7217c51035d11
Barry Robinson <barry.w.robinson64@gmail.com>
2023-07-16 04:24:09 +0100

    add a test rule for yaml parsing

---
+++ parser/rules.py
@@ -0,0 +1,37 @@
+import sys
+import yaml
+
+def load_yaml(FName):
+    f = open(FName)
+    return yaml.load(f, Loader=yaml.FullLoader)
+
+
+def get_patterns_by_id(FName):
+    Y = load_yaml(FName)
+
+    PBI = {}
+    PBN = {}
+    TMap = {}
+    RList = []
+    JList = []
+    TMap = {'json': JList, 'regex':RList}
+    Ptns = Y[0]['patterns']
+    for P in Ptns:
+        PBI[P['id']] = P['pattern']
+        PBN[P['name']] = P['pattern']
+        if P['type'] == 'regex':
+            RList.append(P['pattern'])
+        elif P['type'] == 'json':
+            JList.append(P['pattern'])
+    
+    return [PBI,PBN,TMap]
+
+def main(file):
+    x = get_patterns_by_id(file)
+    print( x )
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2: 
+        print('please supply a file to parse')
+    else: 
+        main(sys.argv[1])
