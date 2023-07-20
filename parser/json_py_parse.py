61b186480f146e5bc03403b3d99e94ab055135dc
Barry Robinson <barry.w.robinson64@gmail.com>
2023-07-20 10:00:12 +0100

    add contains method for contains field

--- parser/json_py_parse.py
+++ parser/json_py_parse.py
@@ -1,10 +1,9 @@
-# Simple python test for json
+#!/usr/bin/env python3
 
+# Simple python test for json
 import json
 import sys
 import copy
-
-
 
 def make_key(current,key):
     c = ''
@@ -12,13 +11,11 @@ def make_key(current,key):
         return "".join(['.',key])
     else:
         return "".join([current,'.',key])
-
-
+    
 # Render the json map into a searchable jq structure
 # this can be indexed simply using jq paths such as 
 # .name.some_name[idx].value 
 def interate_through_dict(Dict, current):
-    print(Dict)
     HashDict = {}
     for key,value in Dict.items():
         if type(value) is str:
@@ -49,16 +46,27 @@ def interate_through_dict(Dict, current):
 def parse_json(JSon):
     Dict = json.loads(JSon)
     HDict = interate_through_dict(Dict,'.')
-    print(HDict)
+    return HDict
 
-def main(JSon):
-    parse_json(JSon)
+def contains(HDict,Path,Value):
+    if Path[-2:] != '[]': 
+        raise ValueError(f"Not an array: {Path}")
+    
+    L = HDict[Path]
+    if Value in L:
+        return True
+
+    return False
+    
+
+def main(JSon, key):
+    P = parse_json(JSon)
+    print(key,'=>',P.get(key))
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('please supply a json string to parse')
+    if len(sys.argv) != 3:
+        print('please supply a json string to parse and a JQ PAth to find in the JSON')
     else: 
-        print(sys.argv[1])
-        main(sys.argv[1])
+        main(sys.argv[1],sys.argv[2])
 
-parse_json( '{"name":"Barry Robinson","dob":"5-10-1964", "roles":{"software engineering":"team lead","skils":["java","c++","python",{"c++":"excelent","java":"avarage","python":"beginer"}]}}' )
+
